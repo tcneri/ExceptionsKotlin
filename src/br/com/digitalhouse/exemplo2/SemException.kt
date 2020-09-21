94042377aac65539acf9cfa9eb88743f7524dd2a
TatuSilva <tainamoraes.br@gmail.com>
2020-09-21 16:37:12 -0300

    Add readLine()

--- src/br/com/digitalhouse/exemplo2/SemException.kt
+++ src/br/com/digitalhouse/exemplo2/SemException.kt
@@ -1,10 +1,21 @@
-//package br.com.digitalhouse.exemplo2
-//
-//fun main(){
-//    val lista = mutableListOf<String>(null)
-//    lista.add("Pato")
-//    lista.add("Cachorro")
-//    lista.add("Gato")
-//    print("Conteúdo da posição 2: ${lista.get(2)}")
-//
-//}
+package br.com.digitalhouse.exemplo2//package br.com.digitalhouse.exemplo2
+
+fun main(){
+    val lista = mutableListOf<String?>("")
+
+    println("Digite um número e receba um bicho:")
+
+    var posicao: String? = readLine()
+
+    try {
+        lista.add("Pato")
+        lista.add("Cachorro")
+        lista.add("Gato")
+        if (posicao != null) {
+            print("Conteúdo da posição $posicao: ${lista.get(posicao.toInt())}")
+        }
+    }catch (ex: Exception){
+        println("Vixi... Deu ruim... Tente um novo número")
+    }
+
+}
